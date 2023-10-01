756f095e31e5eb186801e119dc9a22ce46ed9117
maruware <me@maruware.com>
2023-10-01 16:49:13 +0900

    fix parser

---
+++ lexer_test.go
@@ -0,0 +1,44 @@
+package fscli
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLexer(t *testing.T) {
+	tests := []struct {
+		desc  string
+		input string
+		want  []Token
+	}{
+		{
+			desc:  "simple query",
+			input: `QUERY user WHERE name = "John Doe"`,
+			want: []Token{
+				{Type: QUERY, Literal: "QUERY"},
+				{Type: IDENT, Literal: "user"},
+				{Type: WHERE, Literal: "WHERE"},
+				{Type: IDENT, Literal: "name"},
+				{Type: EQ, Literal: "="},
+				{Type: STRING, Literal: "John Doe"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			l := NewLexer(tt.input)
+			tokens := []Token{}
+			for {
+				tok := l.NextToken()
+				if tok.Type == EOF {
+					break
+				}
+				tokens = append(tokens, tok)
+			}
+
+			assert.Equal(t, tt.want, tokens)
+		})
+	}
+}
--- parser.go
+++ parser.go
@@ -1,30 +1,112 @@
 package fscli
 
 import (
+	"fmt"
 	"strconv"
 )
 
-func Parse(input string) ([]Token, error) {
-	l := NewLexer(input)
-	tokens := []Token{}
-	for {
-		tok := l.NextToken()
-		if tok.Type == EOF {
-			break
-		}
-		tokens = append(tokens, tok)
+type Parser struct {
+	l         *Lexer
+	curToken  Token
+	peekToken Token
+	errors    []string
+}
+
+func NewParser(l *Lexer) *Parser {
+	p := &Parser{l: l, errors: []string{}}
+
+	// Set both curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) Parse() Operation {
+
+	if p.curTokenIs(QUERY) {
+		return p.parseQueryOperation()
 	}
-	return tokens, nil
+	return nil
 }
 
-func parseInt(word string) (int, error) {
-	return strconv.Atoi(word)
+func (p *Parser) parseQueryOperation() *QueryOperation {
+	op := &QueryOperation{opType: QUERY}
+
+	if !p.expectPeek(IDENT) {
+		return nil
+	}
+
+	op.collection = p.curToken.Literal
+
+	if !p.expectPeek(WHERE) {
+		return nil
+	}
+
+	p.nextToken()
+	for !p.curTokenIs(EOF) {
+		filter, err := p.parseFilter()
+		if err != nil {
+			p.errors = append(p.errors, err.Error())
+			return nil
+		}
+		if filter != nil {
+			op.filters = append(op.filters, filter)
+		}
+		p.nextToken()
+	}
+
+	return op
 }
 
-func parseFloat(word string) (float64, error) {
-	return strconv.ParseFloat(word, 64)
+func (p *Parser) parseFilter() (Filter, error) {
+	field := p.curToken.Literal
+
+	p.nextToken()
+	operator := p.curToken.Literal
+
+	p.nextToken()
+
+	if p.curTokenIs(INT) {
+		n, err := strconv.Atoi(p.curToken.Literal)
+		if err != nil {
+			return nil, fmt.Errorf("invalid int value: %s", p.curToken.Literal)
+		}
+		return &IntFilter{field: field, operator: operator, value: n}, nil
+	}
+	if p.curTokenIs(STRING) {
+		return &StringFilter{field: field, operator: operator, value: p.curToken.Literal}, nil
+	}
+	return nil, fmt.Errorf("invalid filter value: %s", p.curToken.Literal)
 }
 
-func parseString(word string) (string, error) {
-	return strconv.Unquote(word)
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) curTokenIs(t TokenType) bool {
+	return p.curToken.Type == t
+}
+func (p *Parser) peekTokenIs(t TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) peekError(t TokenType) {
+	msg := fmt.Sprintf("expected next token tobe %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }

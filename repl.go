9c3de6bd86e61f411065fab856056f646f6cd590
maruware <me@maruware.com>
2023-10-07 13:31:03 +0900

    Support table output

--- repl.go
+++ repl.go
@@ -8,17 +8,43 @@ import (
 	"strings"
 
 	"cloud.google.com/go/firestore"
-	"github.com/mattn/go-colorable"
-
 	"github.com/fatih/color"
+	"github.com/mattn/go-colorable"
 	"github.com/nyaosorg/go-readline-ny"
 	"github.com/nyaosorg/go-readline-ny/coloring"
 	"github.com/nyaosorg/go-readline-ny/simplehistory"
+	"github.com/olekukonko/tablewriter"
+	"golang.org/x/exp/slices"
 )
 
 const LongLine = "--------------------------------------------------------------------------"
 
-func ReplStart(ctx context.Context, fs *firestore.Client, in io.Reader, out io.Writer) {
+type OutputMode string
+
+const (
+	OutputModeJSON  OutputMode = "json"
+	OutputModeTable OutputMode = "table"
+)
+
+type Repl struct {
+	ctx        context.Context
+	fs         *firestore.Client
+	in         io.Reader
+	out        io.Writer
+	outputMode OutputMode
+}
+
+func NewRepl(ctx context.Context, fs *firestore.Client, in io.Reader, out io.Writer, outputMode OutputMode) *Repl {
+	return &Repl{
+		ctx:        ctx,
+		fs:         fs,
+		in:         in,
+		out:        out,
+		outputMode: outputMode,
+	}
+}
+
+func (r *Repl) Start() {
 	history := simplehistory.New()
 
 	editor := &readline.Editor{
@@ -32,10 +58,10 @@ func ReplStart(ctx context.Context, fs *firestore.Client, in io.Reader, out io.W
 		HistoryCycling: true,
 	}
 
-	executor := NewExecutor(ctx, fs)
+	executor := NewExecutor(r.ctx, r.fs)
 
 	for {
-		line, err := editor.ReadLine(ctx)
+		line, err := editor.ReadLine(r.ctx)
 		if err != nil {
 			if err == io.EOF {
 				return
@@ -45,7 +71,7 @@ func ReplStart(ctx context.Context, fs *firestore.Client, in io.Reader, out io.W
 		}
 
 		if strings.TrimSpace(line) == "\\d" {
-			listUpCollections(ctx, fs, out)
+			listUpCollections(r.ctx, r.fs, r.out)
 			continue
 		}
 
@@ -62,39 +88,104 @@ func ReplStart(ctx context.Context, fs *firestore.Client, in io.Reader, out io.W
 			continue
 		}
 		if op, ok := op.(*QueryOperation); ok {
-			docs, err := executor.ExecuteQuery(ctx, op)
+			docs, err := executor.ExecuteQuery(r.ctx, op)
 			if err != nil {
-				fmt.Fprintf(out, "error: %s\n", err)
+				fmt.Fprintf(r.out, "error: %s\n", err)
 				continue
 			}
 
-			for _, doc := range docs {
-				fmt.Fprintf(out, "ID: %s\n", doc.Ref.ID)
-
-				j, err := json.Marshal(doc.Data())
-				if err != nil {
-					fmt.Printf("invalid data: %s\n", err)
-					fmt.Fprintln(out, LongLine)
-					continue
+			if r.outputMode == OutputModeJSON {
+				for _, doc := range docs {
+					r.outputDocJSON(doc)
+					fmt.Fprintln(r.out, LongLine)
 				}
-				fmt.Fprintf(out, "Data: %s\n", j)
-				fmt.Fprintln(out, LongLine)
+			} else if r.outputMode == OutputModeTable {
+				r.outputDocsTable(docs)
 			}
 		}
 		if op, ok := op.(*GetOperation); ok {
-			result, err := executor.ExecuteGet(ctx, op)
+			doc, err := executor.ExecuteGet(r.ctx, op)
 			if err != nil {
-				fmt.Fprintf(out, "error: %s\n", err)
+				fmt.Fprintf(r.out, "error: %s\n", err)
 				continue
 			}
-			j, err := json.Marshal(result)
-			if err != nil {
-				fmt.Fprintf(out, "error: %s\n", err)
-				continue
+
+			if r.outputMode == OutputModeJSON {
+				r.outputDocJSON(doc)
+			} else if r.outputMode == OutputModeTable {
+				r.outputDocTable(doc)
 			}
-			fmt.Fprintf(out, "%s\n", j)
 		}
 	}
+}
+
+func (r *Repl) outputDocJSON(doc *firestore.DocumentSnapshot) {
+	fmt.Fprintf(r.out, "ID: %s\n", doc.Ref.ID)
+	j, err := json.Marshal(doc.Data())
+	if err != nil {
+		fmt.Printf("invalid data: %s\n", err)
+		return
+	}
+	fmt.Fprintf(r.out, "Data: %s\n", j)
+}
+
+func (r *Repl) outputDocsTable(docs []*firestore.DocumentSnapshot) {
+	// collect keys
+	keys := []string{"ID"}
+	for _, doc := range docs {
+		for k := range doc.Data() {
+			if !slices.Contains(keys, k) {
+				keys = append(keys, k)
+			}
+		}
+	}
+
+	table := tablewriter.NewWriter(r.out)
+	table.SetHeader(keys)
+
+	for _, doc := range docs {
+		row := []string{}
+		for _, k := range keys {
+			if k == "ID" {
+				row = append(row, doc.Ref.ID)
+				continue
+			}
+			v, ok := doc.Data()[k]
+			if !ok {
+				row = append(row, "")
+				continue
+			}
+			row = append(row, fmt.Sprintf("%v", v))
+		}
+		table.Append(row)
+	}
+	table.Render()
+}
+
+func (r *Repl) outputDocTable(doc *firestore.DocumentSnapshot) {
+	keys := []string{"ID"}
+	for k := range doc.Data() {
+		keys = append(keys, k)
+	}
+
+	table := tablewriter.NewWriter(r.out)
+	table.SetHeader(keys)
+
+	row := []string{}
+	for _, k := range keys {
+		if k == "ID" {
+			row = append(row, doc.Ref.ID)
+			continue
+		}
+		v, ok := doc.Data()[k]
+		if !ok {
+			row = append(row, "")
+			continue
+		}
+		row = append(row, fmt.Sprintf("%v", v))
+	}
+	table.Append(row)
+	table.Render()
 }
 
 func listUpCollections(ctx context.Context, fs *firestore.Client, out io.Writer) {

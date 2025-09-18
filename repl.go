c7b44ce60dc50fcbc9de09e8ae2eb1826d3d1589
Yuichi Sano <yuichi@tilda.co.jp>
2025-09-18 18:57:53 +0900

    refactor: Split ProcessLine method for modularity

--- repl.go
+++ repl.go
@@ -124,71 +124,91 @@ func (r *Repl) promptProcessLine(line string) {
 func (r *Repl) ProcessLine(line string) {
 	lexer := NewLexer(line)
 	parser := NewParser(lexer)
-	result, err := parser.Parse()
+	op, err := parser.Parse()
 	if err != nil {
 		fmt.Fprintf(r.out, "error: %s\n", err)
 		return
 	}
-	if result == nil {
+	if op == nil {
 		return
 	}
 
-	if op, ok := result.(*MetacommandPager); ok {
-		r.enabledPager = op.on
-		return
+	if err := r.executeOperation(op); err != nil {
+		fmt.Fprintf(r.out, "error: %s\n", err)
+	}
+}
+
+func (r *Repl) executeOperation(op any) error {
+	switch v := op.(type) {
+	case *MetacommandPager:
+		return r.handlePager(v)
+	case *MetacommandListCollections:
+		return r.handleListCollections(v)
+	case *QueryOperation:
+		return r.handleQuery(v)
+	case *GetOperation:
+		return r.handleGet(v)
+	case *CountOperation:
+		return r.handleCount(v)
+	default:
+		return fmt.Errorf("unknown operation type")
+	}
+}
+
+func (r *Repl) handlePager(op *MetacommandPager) error {
+	r.enabledPager = op.on
+	return nil
+}
+
+func (r *Repl) handleListCollections(op *MetacommandListCollections) error {
+	cols, err := r.exe.ExecuteListCollections(r.ctx, op)
+	if err != nil {
+		return err
 	}
 
-	if op, ok := result.(*MetacommandListCollections); ok {
-		cols, err := r.exe.ExecuteListCollections(r.ctx, op)
-		if err != nil {
-			fmt.Fprintf(r.out, "error: %s\n", err)
-			return
-		}
+	out, render := r.pagerableOut()
+	for _, col := range cols {
+		fmt.Fprintf(out, "%s\n", col)
+	}
+	return render()
+}
 
-		out, render := r.pagerableOut()
-		for _, col := range cols {
-			fmt.Fprintf(out, "%s\n", col)
-		}
-		if err := render(); err != nil {
-			fmt.Fprintf(r.out, "error: %s\n", err)
-		}
+func (r *Repl) handleQuery(op *QueryOperation) error {
+	docs, err := r.exe.ExecuteQuery(r.ctx, op)
+	if err != nil {
+		return err
 	}
 
-	if op, ok := result.(*QueryOperation); ok {
-		docs, err := r.exe.ExecuteQuery(r.ctx, op)
-		if err != nil {
-			fmt.Fprintf(r.out, "error: %s\n", err)
-			return
-		}
-
-		if r.outputMode == OutputModeJSON {
-			r.outputDocsJSON(docs)
-		} else if r.outputMode == OutputModeTable {
-			r.outputDocsTable(docs)
-		}
+	if r.outputMode == OutputModeJSON {
+		r.outputDocsJSON(docs)
+	} else if r.outputMode == OutputModeTable {
+		r.outputDocsTable(docs)
 	}
-	if op, ok := result.(*GetOperation); ok {
-		doc, err := r.exe.ExecuteGet(r.ctx, op)
-		if err != nil {
-			fmt.Fprintf(r.out, "error: %s\n", err)
-			return
-		}
+	return nil
+}
 
-		if r.outputMode == OutputModeJSON {
-			r.outputDocJSON(doc)
-		} else if r.outputMode == OutputModeTable {
-			r.outputDocTable(doc)
-		}
+func (r *Repl) handleGet(op *GetOperation) error {
+	doc, err := r.exe.ExecuteGet(r.ctx, op)
+	if err != nil {
+		return err
 	}
-	if op, ok := result.(*CountOperation); ok {
-		count, err := r.exe.ExecuteCount(r.ctx, op)
-		if err != nil {
-			fmt.Fprintf(r.out, "error: %s\n", err)
-			return
-		}
 
-		fmt.Fprintf(r.out, "%d\n", count)
+	if r.outputMode == OutputModeJSON {
+		r.outputDocJSON(doc)
+	} else if r.outputMode == OutputModeTable {
+		r.outputDocTable(doc)
 	}
+	return nil
+}
+
+func (r *Repl) handleCount(op *CountOperation) error {
+	count, err := r.exe.ExecuteCount(r.ctx, op)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(r.out, "%d\n", count)
+	return nil
 }
 
 func (r *Repl) ProcessLineFromPipe() {
